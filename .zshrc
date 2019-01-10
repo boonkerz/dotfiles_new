ed3c84947d18c0071b8d234d14fa9ad5f0ae76da
Thomas Peterson <info@thomas-peterson.de>
2019-01-10 11:32:06 +0100

    some stuff

---
+++ .zshrc
@@ -0,0 +1,13 @@
+export ZSH="/home/thomas/.oh-my-zsh"
+
+ZSH_THEME="robbyrussell"
+
+plugins=(
+  git
+)
+
+source $ZSH/oh-my-zsh.sh
+
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+export GOPATH=$HOME/go
+
--- install.sh
+++ install.sh
@@ -3,4 +3,16 @@
 # Thomas Peterson
 # Install
 
+mkdir -p $HOME/.config/i3blocks
+
+
+rm -f $HOME/.zprofile && ln -s $(pwd)/.zprofile $HOME/.zprofile
+rm -f $HOME/.Xresources && ln -s $(pwd)/.Xresources $HOME/.Xresources
+rm -f $HOME/.zshrc && ln -s $(pwd)/.zshrc $HOME/.zshrc
+
+mv $HOME/.config/i3blocks/config $HOME/.config/i3blocks/config.old && ln -s $(pwd)/.config/i3blocks/config $HOME/.config/i3blocks/config
+
+rm -f $HOME/.scripts
+ln -s $(pwd)/.scripts $HOME/.scripts
+
 mv $HOME/.config/i3/config $HOME/.config/i3/config.old && ln -s $(pwd)/.config/i3/config $HOME/.config/i3/config
